752aad55b8863b1a757e9844a7bea651c326c223
Jules PUGET <96185316+Julespgt@users.noreply.github.com>
2024-04-18 20:46:05 +0200

    Controller-Compare-Password (#43)
    * Controller-Compare-Password
    
    * Mise à jour de la
    
    * Mise à jour de la méthode OnSignIn

--- src/Controller/SignIn/ControllerSignIn.java
+++ src/Controller/SignIn/ControllerSignIn.java
@@ -1,5 +1,6 @@
 package Controller.SignIn;
 
+import Controller.SignUp.Hash;
 import Model.User.User;
 import Model.User.UserDaoImpl;
 import View.PopUpMessage;
@@ -7,6 +8,7 @@ import View.PopUpMessage;
 public class ControllerSignIn {
     public boolean onSignIn(String email, String password) {
         UserDaoImpl userDao = new UserDaoImpl();
+        Hash hash = new Hash();
         PopUpMessage popUpMessage = new PopUpMessage();
         System.out.println(email+" "+password);
         boolean mailExists = userDao.DoesMailExist(email);
@@ -15,12 +17,14 @@ public class ControllerSignIn {
             popUpMessage.showErrorMessage("L'adresse e-mail n'existe pas.");
             return false; // Indiquer que la connexion a échoué
         }else{
-            if(!passwordExists){
+            User user = userDao.getUserByMail(email);
+            boolean passwordCorrect = hash.comparePassword(password,user.getUser_password());
+            if(!passwordCorrect){
                 popUpMessage.showErrorMessage("Le mot de passe est incorrect.");
                 return false; // Indiquer que la connexion a échoué
             }else{
-                User user = userDao.getUserByMail(email);
                 //popUpMessage.showSuccessMessage("Connexion réussie."+user.toString());
+
                 return true;
             }
         }
